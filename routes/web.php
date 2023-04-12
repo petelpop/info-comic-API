eb030d1d9b9a0021d573ffa8fd273ba41e177d27
ipal <72238450+petelpop@users.noreply.github.com>
2023-04-12 21:37:25 +0700

    update some mistake

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\ComicController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/comics', [ComicController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
