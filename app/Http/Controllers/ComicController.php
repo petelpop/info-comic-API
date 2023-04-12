c8fd2fa2dc0ba92e24c326b73764b6d6ce0c6b72
ipal <72238450+petelpop@users.noreply.github.com>
2023-04-12 21:35:48 +0700

    make comic resource

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 use App\Models\Comic;
 use Illuminate\Http\Request;
-
+use App\Http\Resources\ComicResource;
 class ComicController extends Controller
 {
     public function index(){
         $comics = Comic::all();
-        return response()->json($comics);
+        return ComicResource::collection($comics);        
     }
 }
---
+++ app/Http/Resources/ComicResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ComicResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this -> id,
+            'title' => $this -> title,
+            'prolog' => $this -> prolog,
+            'author_id' => $this -> author,
+            'episode' => $this -> eps,
+            'created_at' => date_format($this->created_at, "Y/m/d H:i:s"),
+        ];
+    }
+}
